5bc8c66ead4b274397547300a3672f4648ca3468
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-28 09:32:46 -0800

    Added else if statements to the renderLicenseLink to return the license link selected.

--- script/markdown/generateMarkdown.js
+++ script/markdown/generateMarkdown.js
@@ -16,17 +16,32 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+  return 'MIT';
+  } else if (license === 'Apache 2.0') {
+  return 'Apache 2.0';
+  } else if (license === 'GPL 3.0') {
+  return 'GPL 3.0';
+  } else if (license === 'BSD 3') {
+  return 'BSD 3';
+  } else {
+  return '';
+  }
+  }
+  
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+  if (license) {
+  return `
+  
+  License
+  This project is licensed under the ${renderLicenseLink(license)} license.`
+  } else {
+  return '';
+  }
+  }
+  
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {}
