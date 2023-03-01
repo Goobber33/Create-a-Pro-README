acd3a7c0b875ae70147f27863d4037e0928aeabc
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-01 09:54:59 -0800

    Finished adding descriptive comments to the generateMarkdown.js file. Comments explain the functions, and their functionality for the app.

--- script/markdown/generateMarkdown.js
+++ script/markdown/generateMarkdown.js
@@ -24,36 +24,45 @@ function renderLicenseBadge(license) {
 
 function renderLicenseLink(license) {
   if (license === 'MIT') {
-  return 'MIT';
+    return 'MIT';
   } else if (license === 'Apache 2.0') {
-  return 'Apache 2.0';
+    return 'Apache 2.0';
   } else if (license === 'GPL 3.0') {
-  return 'GPL 3.0';
+    return 'GPL 3.0';
   } else if (license === 'BSD 3') {
-  return 'BSD 3';
+    return 'BSD 3';
   } else {
-  return '';
+    return '';
   }
-  }
-  
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+// This function takes in a license type as a parameter and returns a formatted license section for the README file. If no license is passed in, it returns an empty string.
+
+function renderLicenseSection(license) {
   if (license) {
-  return `
+    return `
   
   License
   This project is licensed under the ${renderLicenseLink(license)} license.`
   } else {
-  return '';
+    return '';
   }
-  }
-  
-  // TODO: Create a function to generate markdown for README
-  function generateMarkdown(data) {
+}
+
+// TODO: Create a function to generate markdown for README
+
+// This function takes in an object of data that contains information for the README file and generates a markdown formatted output string for the file.
+
+function generateMarkdown(data) {
   const licenseBadge = renderLicenseBadge(data.license);
   const licenseSection = renderLicenseSection(data.license);
-  
+
+
+  // Return a markdown formatted string that includes the data from the data object, the license badge image link and license section obtained from the renderLicenseBadge and renderLicenseSection functions
+
   return `# ${data.title}
   
   ${licenseBadge}
@@ -82,6 +91,8 @@ function renderLicenseLink(license) {
   If you have any questions, feel free to reach out to me at ${data.email}. You can also check out my GitHub profile for more of my projects.
   
   `;
-  }
-  
-  module.exports = generateMarkdown;
+}
+
+// Export the generateMarkdown function for use in other files
+
+module.exports = generateMarkdown;
